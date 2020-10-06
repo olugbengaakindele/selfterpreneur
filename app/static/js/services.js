aefe1a0c6e2a59622283a414159276c8843d17d2
olugbenga Akindele <akindelegbenga@gmail.com>
2020-10-05 22:47:23 -0400

    services 50

---
+++ app/static/js/services.js
@@ -0,0 +1,17 @@
+/*   
+    1- Declare empty list 
+    2- Declare empty object
+    3- populate object and append to list 
+    4- JSON.stringfy the list then you have JSOn
+    6- Send the JSON data to API
+    7- from API route send to data base 
+
+*/
+
+// Service will contain all the service of a user in a list of objects
+let all_services =[];
+// service contains description of each service
+let services ={};
+
+
+
